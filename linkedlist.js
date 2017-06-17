512dfabf25f3d8742887436dfe8a310f2e952039
Emmanuel <emmanueljucaban@gmail.com>
2017-06-17 12:37:40 -0700

    Add addToTail method to LinkedList prototype

--- linkedlist.js
+++ linkedlist.js
@@ -14,7 +14,7 @@ var Node = function(value, next, prev) {
 LinkedList.prototype.addToHead = function(value){
   // create the new node. Set its next value to the current head
   var newNode = new Node(value, this.head, null);
-  if(this.head){
+  if (this.head){
     // set the current head as the next
     this.head.prev = newNode;
   } else {
@@ -22,3 +22,27 @@ LinkedList.prototype.addToHead = function(value){
   }
   this.head = newNode;
 };
+
+
+LinkedList.prototype.addToTail = function(value){
+  // create a new Node with its next set to null and previous set to the tail
+  var newNode = new Node(value, null, this.tail);
+  // Check if tail already exist
+  if (this.tail) {
+    // Set the current tails next property to the new node
+    this.tail.next = newNode
+  } else {
+    // if tails doesnt exist, set the head as the new node
+    this.head = newNode;
+  }
+  this.tail = newNode;
+};
+
+
+var ll = new LinkedList();
+
+ll.addToHead(200);
+ll.addToHead(100);
+ll.addToTail(300);
+
+console.log(ll);
